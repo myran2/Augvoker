cca97aa814796ff8598d3a19eff98e0fff98c80b
Henry Gordon <henrytgordon@gmail.com>
2023-09-05 20:51:14 -0400

    forgot to round

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -157,7 +157,7 @@ export default defineComponent({
 
         secondsToTime(seconds: number): string {
             const min = Math.floor(seconds / 60);
-            const sec = seconds % 60;
+            const sec = Math.floor(seconds % 60);
             return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
         },
 
