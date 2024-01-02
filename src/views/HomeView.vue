e5b593507066f30de42acca47b382a78d70d8913
Henry Gordon <henrytgordon@gmail.com>
2024-01-02 18:21:58 -0500

    move ability blacklist into its own constant
    * also prep boss time interval skips for a similar thing
    * also grab top 3 damagers per interval

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -67,6 +67,7 @@ import Textarea from 'primevue/textarea';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Message from 'primevue/message';
+import { blacklistedAbilities } from '@/constants/BlacklistedAbilities';
 
 interface WclDamager {
     name: string;
@@ -114,71 +115,8 @@ export default defineComponent({
             tableValues: [],
             loading: false,
             mrtNote: "",
-            ignoreSpellIds: [
-            409632, // Breath of Eons
-            402583, // Beacon
-            408682, // Dragonfire Bomb Dispenser
-            408694, // Dragonfire Bomb Dispenser
-            401324, // Pocket Anvil (Echoed Flare)
-            401306, // Pocket Anvil (Anvil Strike)
-            401422, // Vessel of Searing Shadow (Shadow Spike)
-            401428, // Vessel of Searing Shadow (Ravenous Shadowflame)
-            418774, // Mirror of Fractured Tomorrows ()
-            418588, // Mirror of Fractured Tomorrows (Sand Cleave)
-            419591, // Mirror of Fractured Tomorrows (Auto Attack)
-            418607, // Mirror of Fractured Tomorrows (Sand Bolt)
-            406251, // Roiling Shadowflame
-            406889, // Roiling Shadowflame (Self Harm)
-            379403, // Toxic Thorn Footwraps (Launched Thorns)
-            408791, // Ashkandur, Fall of the Brotherhood
-            378426, // Slimy Expulsion Boots boots (Corrosive Slime)
-            381006, // Acidic Hailstone Treads (Deep Chill)
-            381700, // Forgestorm (Forgestorm Ignited)
-            406764, // Shadowflame Wreathe
-            394453, // Broodkeeper's Blaze
-            370794, // Unstable Frostfire Belt (Lingering Frostspark)
-            408836, // Djaruun, Pillar of the Elder Flame
-            408815, // Djaruun, Pillar of the Elder Flame
-            381475, // Erupting Spear Fragment
-            281721, // Bile-Stained Crawg Tusks (Vile Bile)
-            214397, // Mark of Dargrul (Landslide)
-            408469, // Call to Suffering (Self Harm)
-            374087, // Glacial Fury
-            370817, // Shocking Disclosure
-            426564, // Augury of the Primal Flame (Annihilating Flame)
-            417458, // Accelerating Sandglass
-            424965, // Thorn Spirit
-            425181, // Thorn Blast
-            419737, // Timestrike
-            265953, // Touch of Gold
-            425154, // Vicious Brand
-            425156, // Radiating Brand
-            422146, // Solar Maelstrom
-            426341, // Tindral's Fowl Fantasia
-            426431, // Denizen Of The Flame
-            426486, // Denizen Of The Flame Final
-            426339, // Igiras Cruel Nightmare
-            426527, // Flaying Torment
-            259756, // Entropic Embrace
-            427209, // Web of Dreams
-            422956, // Essence Splice
-            424324, // Hungering Shadowflame
-            419279, // Extinction Blast
-            215444, // Dark Blast
-            214168, // Brutal Haymaker
-            214169, // Brutal Haymaker
-            228784, // Brutal Haymaker Vulnerability
-            214350, // Nightmare Essence
-            422750, // Shadowflame Rage
-            425701, // Shadowflame Lash Enemy
-            422750, // Tainted Heart
-            425461, // Tainted Heart Enemy Damage
-            417458, // Accelerating Sandglass
-            215407, // Dark Blast
-            270827, // Webweavers Soul Gem
-            213785, // Nightfall
-            ],
-            damagersPerRow: 2,
+            ignoreSpellIds: blacklistedAbilities,
+            damagersPerRow: 3,
         };
     },
     methods: {
@@ -191,9 +129,20 @@ export default defineComponent({
             this.tableValues = [];
             this.topDamagersByTime = [];
 
-            if (this.fight && this.fight.name == "Scalecommander Sarkareth") {
-                // Prefill skip intervals for P1->P2 and P2->P3 intermissions by default.
-                this.skipTimeIntervals = [{ start: 105, end:135 }, { start: 235, end: 255 }];
+            // prefill some mythic fight skip intervals
+            if (this.fight && this.fight.difficulty == 5) {
+                switch(this.fight.boss) {
+                    // Scalecommander Sarkareth
+                    case 2685:
+                        // P1->P2 and P2->P3 intermissions.
+                        this.skipTimeIntervals = [
+                            // P1 -> P2 intermission
+                            { start: 105, end:135 },
+                            // P2 -> P3 intermission
+                            { start: 235, end: 255 }
+                        ];
+                        break;
+                }
             }
         },
 
